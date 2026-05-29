42bb2e96553df839dd77630431c45240051d3785
Tahsin Sekizkardesler <tahsinsekizkardesler@gmail.com>
2026-05-29 20:35:38 +0200

    Add meal type model

--- lib/models/meal_type.dart
+++ lib/models/meal_type.dart
@@ -1,1 +1,14 @@
+enum MealType { breakfast, lunch, dinner }
 
+extension MealTypeLabel on MealType {
+  String get label {
+    switch (this) {
+      case MealType.breakfast:
+        return 'Breakfast';
+      case MealType.lunch:
+        return 'Lunch';
+      case MealType.dinner:
+        return 'Dinner';
+    }
+  }
+}
